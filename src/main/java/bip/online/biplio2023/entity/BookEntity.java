0dd39c84d85b5181e1509f49bd06a68f3913dd6d
Krapivin <tailsforhire229@gmail.com>
2024-04-15 13:40:17 +0300

    WORK SERVER123

--- src/main/java/bip/online/biplio2023/entity/BookEntity.java
+++ src/main/java/bip/online/biplio2023/entity/BookEntity.java
@@ -1,5 +1,6 @@
 package bip.online.biplio2023.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
